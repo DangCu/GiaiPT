5fca19390ca400a37010aac9037781553fbafc86
cdang5969@gmail.com <cdang5969@gmail.com>
2021-09-14 08:58:20 +0700

    9/14

--- GiaiPT/Controllers/DemoController.cs
+++ GiaiPT/Controllers/DemoController.cs
@@ -3,25 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GiaiPT.Models;
 
-namespace GiaiPT.Controllers
+namespace PTPMQL.Controllers
 {
     public class DemoController : Controller
     {
+
         // GET: Demo
-        public ActionResult Giaipt()
+        public ActionResult bailam()
         {
             return View();
-           
         }
         [HttpPost]
-        
-
+        public ActionResult bailam(string soA, string soB)
         {
-
+            
         }
-
-
-
     }
 }
